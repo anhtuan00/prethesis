3e0053424019fffc3682e0ce3fb5f87b6c26e8d6
anhtuan00 <67965462+anhtuan00@users.noreply.github.com>
2022-12-18 20:48:52 +0700

    Update index.js

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -2,4 +2,5 @@ import AddJob from "./AddJob";
 import AllJobs from "./AllJobs";
 import Profile from "./Profile";
 import SharedLayout from "./SharedLayout";
+import AdminHomePage from "./AdminHomePage";
 export { AllJobs, Profile, SharedLayout, AddJob, AdminHomePage };
